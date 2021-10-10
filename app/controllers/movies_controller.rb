035e6928e902b57e4b5fd6165286c7710cd6bf26
Pelin Cetin <pc2807@columbia.edu>
2021-10-10 03:30:13 +0000

    let's see

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,8 +8,9 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-    session[:ratings] = params[:ratings] unless params[:ratings].nil?
-    @ratings_to_show = session[:sort] || params[:sort] ||{}
+    @ratings_to_show = session[:ratings] || params[:ratings] || {}
+    session[:sort] = params[:sort] unless params[:sort].nil?
+    
     if @ratings_to_show.nil?
         @ratings_to_show = Hash[@all_ratings.map {|rating| [rating, 1]}]
     end
@@ -22,7 +23,7 @@ class MoviesController < ApplicationController
     end
     
     if !params[:ratings].nil?
-      return @movies = Movie.with_ratings(params[:ratings].keys).order(session[:sort])
+      return @movies = Movie.with_ratings(@ratings_to_show.keys).order(session[:sort])
     else
       return @movies = Movie.all.order(session[:sort])
     end
